faca1e36bdd38560bb88383e74b8456a5f8adcf1
Phillip Galbraith <81811191+topstock@users.noreply.github.com>
2022-12-12 22:25:56 -0800

    fixed images

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ import {
   BsEnvelope,
   BsGithub,
  } from "react-icons/bs";
+ import {
+  RiReactjsFill,
+ } from "react-icons/ri";
+import {  SiRedux } from 'react-icons/si';
 import { useEffect } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -27,7 +31,7 @@ export default function Home() {
         <title>
           Phillip Galbraith
         </title>
-        <link href="/logo.png" rel="icon" />
+        <link href="/icon.png" rel="icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div id="home"></div>
@@ -35,7 +39,7 @@ export default function Home() {
         <Navbar expand="lg" fixed="top" bg="secondary gradient">
           <Container >
             <Navbar.Brand href="#home">    
-            <img src="./logo.png" className=""/>  
+            <img src="./bitmap.png" className=""/>  
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
@@ -67,68 +71,77 @@ export default function Home() {
       <main className="bg-gradient text-black mt-5">
         <div className="bg-success bg-gradient">
 
-
-          <h1 className='pt-5 ps-5'>Phillip Galbraith</h1>
-  
-          <div className="container col">
-            <div className="row">
+          <div className="container">
+            <h1 className='pt-5 '>Phillip Galbraith</h1>
+    
+            <div className="container col">
               <div className="row">
-     
-                <div className="row mb-3">
-                 
-                  <div id="about" className='col-lg pe-5 me-5'>
-                    <p>
-                      I make, modify, and modularize 
-                      APIs, responsive pages, 
-                      and graphics.
-                    </p>
-                    <p className="d-flex text-secondary"> 
-                      <div className="p-2 bg-white rounded-circle border border-white" >
-                        <a href="https://expressjs.com" target="_blank" rel="noreferrer"> 
-                          <img src="js.png" alt="html5" width="40" height="40"/> 
-                        </a>
+                <div className="row">
+       
+                  <div className="row mb-3">
+                   
+                    <div id="about" className='col-lg pe-5 me-5'>
+                      <p className="fs-4">
+                        I make, modify, and modularize 
+                        APIs, responsive pages, 
+                        and graphics in the greater Seattle area. 
+                      </p>
+                      <div className=" px-1 d-flex container text-secondary link-white">
+                        <div className='d-flex row w-100'> 
+                          <div className=" text-center col-1 p-2 bg-white rounded-circle border border-white" >
+                            <a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript" target="_blank" rel="noreferrer"> 
+                              <b className="fs-1 text-dark">JS</b>
+                            </a>
+                          </div>
+                          
+                          <div className="mx-1 col-1 p-2 bg-white rounded-circle border border-white fs-7" >
+                            <a href="https://expressjs.com" target="_blank" rel="noreferrer"> 
+                             <div className="fs-4"> <br/></div>
+                              <b className="fs-7 text-dark">express</b>
+                            </a>
+                          </div>
+                          
+                          <div className="col-1 p-2 bg-white text-center  rounded-circle border border-white" >
+                            <a href="https://reactjs.org/" target="_blank" rel="noreferrer" className='link-dark'> 
+                              <div className="fs-4"><RiReactjsFill /></div>
+                              <div className='fs-6'> react </div>
+    
+                            </a> 
+                          </div>
+                          <div className="col-1 mx-1 p-2 bg-white text-center rounded-circle border border-white fs-1" >
+                            <a href="https://www.postgresql.org" target="_blank" rel="noreferrer" className='link-dark'> 
+    
+                              <div className="fs-4">🐘</div>
+                              <div className='fs-6'>postgres</div>
+                            </a>
+                          </div>
+                          <div className="col-1 text-center p-2 bg-white rounded-circle border border-white" >
+                            <a className='link-dark' href="https://redux.js.org" target="_blank" rel="noreferrer"> 
+                              <div className="fs-6"><SiRedux /></div>
+                              <div className="fs-6">redux</div>                          
+                            </a> 
+                          </div>
+                        </div>
                       </div>
-                      
-                      <span>{" . "}</span>
-                      <div className="p-2 bg-white rounded-circle border border-white" >
-                        <a href="https://expressjs.com" target="_blank" rel="noreferrer"> 
-                          <img src="express.png" alt="html5" width="40" height="40"/> 
-                        </a>
-                      </div>
-                      
-                      <span>{" . "}</span>
-                      <div className="p-2 bg-white rounded-circle border border-white" >
-                        <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> 
-                          <img src="react.png" alt="react" width="40" height="40"/> 
-                        </a> 
-                      </div>
-                      <span>{" . "}</span>
-                      <div className="p-2 bg-white rounded-circle border border-white" >
-                        <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> 
-                          <img src="postgres.png" alt="postgresql" width="40" height="40"/> 
-                        </a>
-                      </div>
-                      <span>{" . "}</span>
-                      <div className="p-2 bg-white rounded-circle border border-white" >
-                        <a className="bg-white rounded-circle border border-white" href="https://redux.js.org" target="_blank" rel="noreferrer"> 
-                          <img src="redux.png" alt="redux" width="40" height="40"/> 
-                        </a> 
-                      </div>
-                    </p>
+                    </div>
+                    
                   </div>
-                  
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div id="portfolio" className="container">
-          <h2>portfolio</h2>
+        <div id="portfolio" className="container  pt-5">
+          <h3>
+            <span className="text-success"> {"{"} </span>         
+            portfolio
+            <span className="text-success"> {"}"} </span>
+          </h3>
           <div className="row">
  
             <div className="col-sm pb-2">
               <a href="https://phillipgalbraith.github.io/portfolio-site/">
-                <div className="card link-info bg-black my-2" >
+                <div className="card link-success bg-black my-2" >
                   <img src="./vanillaPortfolio.png" className="card-img-top" alt="..."/>
                   <div className="card-body">
                     <h5 className="card-title">Portfolio</h5>
@@ -142,7 +155,7 @@ export default function Home() {
             </div>
             <div className="col-sm pb-2">
               <a href="https://marketplace-f9a23.web.app/">
-                <div className="card link-info bg-black  my-2" >
+                <div className="card link-success bg-black  my-2" >
                   <img src="./marketplace.png" className="card-img-top" alt="..."/>
                   <div className="card-body">
                     <h5 className="card-title">Marketplace</h5>
@@ -155,7 +168,7 @@ export default function Home() {
             </div>
             <div className="col-sm row">
               <a href="https://feast-planner-2b2ea.web.app/dashboard">
-                <div className="card link-info bg-black my-2" >
+                <div className="card link-success bg-black my-2" >
                   <img src="./feastPlanner.png" className="card-img-top" alt="..."/>
                   <div className="card-body">
                     <h5 className="card-title">Feast Planner</h5>
@@ -171,7 +184,11 @@ export default function Home() {
         </div>
     
         <div className='container w-100 my-4' id="resume">
-          My Resume
+          <h3>
+            <span className="text-success"> {"{"} </span>
+            My Resume 
+            <span className="text-success"> {"}"} </span>
+          </h3>
           <iframe 
           width="100%"
           height="600px"
@@ -179,37 +196,42 @@ export default function Home() {
           >
           </iframe>
         </div>
-        <div id="contact" className=" pb-5 container row bg-dark text-white">        
-          <Navbar expand="lg">
-            <Container >
-              <Navbar.Brand href="#home">    
-                <span className="text-white">Contacts</span>
-              </Navbar.Brand>
-                <Nav className="me-auto">
-                  <Nav.Link id="home" href="#contacts" >
-                    <span className="text-info">
-                      <BsEnvelope />
-                      {" "}
-                      <b>Email</b> 
-                    </span>
-                  </Nav.Link>
-
-                  <Nav.Link href="https://github.com/phillipgalbraith" >
-                    <span className="text-info">
-                      <BsGithub />
-                      {" "}
-                      <b>GitHub</b> 
-                    </span>
-                  </Nav.Link>  
-                  <Nav.Link href="https://linkedin.com/in/phillip-galbraith-web" >
-                  <span className="text-info">
-                      <BsLinkedin />
-                      {" "}
-                      <b>LinkedIn</b> 
-                    </span>                </Nav.Link>  
-                </Nav>
-            </Container>
-          </Navbar>
+        <div className="container">
+          <div className="row">
+      
+            <div id="contact" className=" pb-5 col-5 text-white">        
+              <Navbar expand="lg">
+                <Container >
+                  <Navbar.Brand href="#home">    
+                    <span className="text-white">Contacts</span>
+                  </Navbar.Brand>
+                    <Nav className="me-auto">
+                      <Nav.Link id="home" href="#contacts" >
+                        <span className="text-white">
+                          <BsEnvelope />
+                          {" "}
+                          <b>Email</b> 
+                        </span>
+                      </Nav.Link>
+    
+                      <Nav.Link href="https://github.com/phillipgalbraith" >
+                        <span className="text-white">
+                          <BsGithub />
+                          {" "}
+                          <b>GitHub</b> 
+                        </span>
+                      </Nav.Link>  
+                      <Nav.Link href="https://linkedin.com/in/phillip-galbraith-web" >
+                      <span className="text-white">
+                          <BsLinkedin />
+                          {" "}
+                          <b>LinkedIn</b> 
+                        </span>                </Nav.Link>  
+                    </Nav>
+                </Container>
+              </Navbar>
+            </div>
+          </div>
         </div>
       </main>
     
